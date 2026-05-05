406a34930e04a507a02534e6faca585ed44f87fb
Joel Miller <millerjoel7597@gmail.com>
2026-05-05 17:15:00 +0100

    Add activeRecipientId to Home component for chat functionality

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { cn } from "@/lib/utils";
 
 export default function Home() {
   const { user, isLoading, privateKey, logout } = useAuth();
+  const { activeRecipientId } = useChat();
 
   if (isLoading) {
     return (
